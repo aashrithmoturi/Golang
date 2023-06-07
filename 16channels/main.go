4ace165356b23373ee4ba4982aa5492f2e735fe6
Aashrith Moturi(mac) <a0m1aq9@homeoffice.wal-mart.com>
2023-06-07 11:00:43 +0530

    Saved a file

--- 16channels/main.go
+++ 16channels/main.go
@@ -1,10 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 func main() {
 	myCh := make(chan int)
-	myCh <- 5
-	fmt.Println(<-myCh)
-
+	// myCh <- 5
+	// fmt.Println(<-myCh)
+	wg := &sync.WaitGroup{}
+	wg.Add(2)
+	go func (wg *sync.WaitGroup, ch chan int){
+		
+	}(wg, myCh)
+	wg.Wait()
 }
