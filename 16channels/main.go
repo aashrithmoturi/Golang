ef739aa3ec5aaab93ca6c626edbd408dadd4eafa
Aashrith Moturi(mac) <a0m1aq9@homeoffice.wal-mart.com>
2023-06-07 12:02:17 +0530

    added a multiple functions into the file

--- 16channels/main.go
+++ 16channels/main.go
@@ -11,8 +11,30 @@ func main() {
 	// fmt.Println(<-myCh)
 	wg := &sync.WaitGroup{}
 	wg.Add(2)
-	go func (wg *sync.WaitGroup, ch chan int){
-		
+
+	//receive only
+	//we cannot use close statements on receive channels
+	go func(wg *sync.WaitGroup, ch <-chan int) {
+		//we must have same number of <-mych as myCh<-
+
+		//If i donot have any senders then fp prints zero which shouldnt be the case
+		//because we might have a case where sender sends a zero in both
+		//cases printing zero doesnt make sense
+		val, isChannelOpen := <-myCh
+		if isChannelOpen == false {
+			return
+		}
+		fmt.Println(val)
+		fmt.Println(<-myCh)
+		wg.Done()
+	}(wg, myCh)
+
+	//send only
+	go func(wg *sync.WaitGroup, ch chan<- int) {
+		myCh <- 5
+		myCh <- 6
+		close(myCh)
+		wg.Done()
 	}(wg, myCh)
 	wg.Wait()
 }
