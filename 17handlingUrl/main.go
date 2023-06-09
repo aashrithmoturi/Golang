80d051e9769e6b513afb74ea9f555543970a20fc
Aashrith Moturi(mac) <a0m1aq9@homeoffice.wal-mart.com>
2023-06-09 10:31:50 +0530

    added a folder for handling urls

---
+++ 17handlingUrl/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"net/url"
+)
+
+const myurl string = "https://lco.dev:3000/learn?coursename=reactjs&paymentid=ghgwuyej"
+
+func main() {
+	fmt.Println("Handling url")
+	fmt.Println(myurl)
+
+	// parsing the url
+	res, _ := url.Parse(myurl)
+	// fmt.Println(res.Scheme)
+	// fmt.Println(res.User)
+	// fmt.Println(res.Host)
+	// fmt.Println(res.Port())
+	// fmt.Println(res.Path)
+	// fmt.Println(res.RawQuery)
+
+	qparams := res.Query()
+	fmt.Printf("%T\n", qparams)
+	// fmt.Println(qparams)
+	for key, value := range qparams {
+		fmt.Println(key, value)
+	}
+
+	partOfUrl := &url.URL{
+		Scheme:   "https",
+		Host:     "lco.dev",
+		Path:     "/qjhwgf",
+		RawQuery: "user=ash",
+	}
+
+	fmt.Println(partOfUrl.String())
+
+}
